7cdbb16eddf628f37ac430d191ea1bd1987f6d04
Adesh <adeshp948@gmail.com>
2023-02-20 06:32:08 -0500

    Creating ThunderClient collections to manage requests

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,27 +4,43 @@ const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 
 
-//create a user using : POST "/api.auth/"
-//doesn't require Auth
-router.post('/',[
+//create a user using : POST "/api/auth/createuser"
+//  No Login required
+router.post('/createuser',[
     body('email','Enter a valid Email').isEmail(),
     body('name','Enter a valid Name').isLength({ min: 3 }),
     body('password','Enter a valid Password').isLength({ min: 5 }),
-],(req,res)=>{
-
+],async(req,res)=>{
+// If there are errors , return bad requests and an error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+
+    //check whether user with same email exists
+    try{
+    
+    
+    let user = await User.findOne({email:req.body.email});
+    if(user) return res.status(400).json({error: " Email already exists"})
+    //creating new user
+    user =  await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
-      }).then(user => res.json(user))
-      .catch(err=>{console.log(err)
-    res.json({error: "Please Enter Unique Email", message:err.message})});
+      })
+      
+    //   .then(user => res.json(user))
+    //   .catch(err=>{console.log(err)
+    // res.json({error: "Please Enter Unique Email", message:err.message})});
 
     // res.send(req.body)
+    res.json(user)
+    }
+    catch(error){ 
+        console.error(error.message)
+        res.status(500).send("Some error occured!!!")
+    }
 }) 
 
 module.exports = router
