636bad0428dbd4637bc07f8968ca45f6c2ff8f73
Adesh <adeshp948@gmail.com>
2023-02-25 20:23:07 -0500

    adding card to show notes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,31 @@
-import React, { useContext } from 'react'
-import noteContext from '../context/notes/noteContext';
+import React from 'react'
+import Notes from './Notes'
 const Home = () => {
-    const context = useContext(noteContext);
-    const {notes,setNotes}=context;
+    
 
     return (
         <div>
         <div className="container my-3">
             <h1>Add a Note </h1>
             <form>
-                <div class="form-group my-3">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <div className="form-group my-3">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
-                <div class="form-group my-3">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <div className="form-group my-3">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                 </div>
-                <div class="form-check my-3">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div className="form-check my-3">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
             </div>
-            <div className="container my-3">
-
-            <h1>Your notes</h1>
-            {notes.map((note)=>{
-                return note.title;
-            })}
-            </div>
+            <Notes/>
+            
         </div>
     )
 }
