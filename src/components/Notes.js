545630928657cc9e3e53335b9dbe03232d4304b8
Adesh <adeshp948@gmail.com>
2023-02-28 07:14:34 -0500

    Adding edit note feature on frontend and backend.

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,58 +1,99 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+//using useRef hook to give reference to our modal variable
 import noteContext from "../context/notes/noteContext";
 import NoteItem from "./NoteItem";
 import AddNote from './AddNote'
 
 
 const Notes = () => {
-    const context = useContext(noteContext);
-    const { notes, getAllNotes } = context;
-    useEffect(() => {
-        getAllNotes()
-        // eslint-disable-next-line
-    }, [])
+  const context = useContext(noteContext);
+  const { notes, getAllNotes, editNote } = context;
+  useEffect(() => {// want to use this as a componentdidmount so i added below commented line
+    getAllNotes()
+    // eslint-disable-next-line
+  }, [])
 
-    const ref = useRef(null)
-    const updateNote = (note) => ref.current.click();
+  const ref = useRef(null)
+  const refClose = useRef(null)
+  const [note, setNote] = useState({ id: "", etitle: "", edescription: "", etag: "default" })
 
-    const first = useRef(null)
+  const updateNote = (currentNote) => {
+    ref.current.click();//clicking on ref button
+    setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag });
+  }
 
-    return (
-        <>
-            <AddNote />
-            <button type="button" class="btn btn-primary" ref={ref} data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
-            </button>
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Edit Note</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
+  const UpdateOnClick = (e) => {
+    editNote(note.id, note.etitle, note.edescription, note.etag)
+    refClose.current.click();
+  }
+
+  // ... is spread operator
+  // means keep tha values which are there in note object and add/overwrite properties listed next.
+  const handleOnChnage = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value })//changing event should change to its value
+  }
+
+  return (
+    <>
+      <AddNote />
+      <button type="button" className="btn btn-primary d-none" ref={ref} data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+      {/* giving reference to this button */}
+      <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
+            {/* ===================================================================================================================== */}
+
+            <div className="modal-body">
+
+              {/* after clicking on Edit note button below form will appeare. */}
+              <div>
+                <div className="container my-3">
+                  <form>
+                    <div className="form-group my-3">
+                      <label htmlFor="title">Title</label>
+                      <input type="text" className="form-control" onChange={handleOnChnage} value={note.etitle} id="etitle" name="etitle" aria-describedby="emailHelp" />
                     </div>
+                    <div className="form-group my-3">
+                      <label htmlFor="description">Description</label>
+                      <input type="text" className="form-control" onChange={handleOnChnage} value={note.edescription} id="edescription" name="edescription" />
+                    </div>
+                    <div className="form-group my-3">
+                      <label htmlFor="description">Tag</label>
+                      <input type="text" className="form-control" onChange={handleOnChnage} value={note.etag} id="etag" name="etag" />
+                    </div>
+                  </form>
                 </div>
+              </div>
             </div>
 
+            {/* ===================================================================================================================== */}
 
-
-            <div className="row my-3">
-                <h1>Your Notes</h1>
-                {notes.map((note) => {
-                    return <NoteItem key={note._id} updateNote={updateNote} note={note} />;
-                })}
+            <div className="modal-footer">
+              <button ref={refClose} type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary" onClick={UpdateOnClick}>Update Note</button>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+
+
+      <div className="row my-3">
+        <h1>Your Notes</h1>
+        {notes.map((note) => {
+          return <NoteItem key={note._id} updateNote={updateNote} note={note} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Notes;
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -5,10 +5,10 @@ const NoteState = (props) => {
     const host = "http://localhost:8000";
     const notesInitial = [];
     const [notes, setNotes] = useState(notesInitial);
-// eslint-disable-next-line
+    // eslint-disable-next-line
 
 
-    // Get all notes
+    // GET all notes
     const getAllNotes = async (title, description, tag) => {
         // api call
         const response = await fetch(`${host}/api/notes/fetchallnotes`, {
@@ -21,13 +21,13 @@ const NoteState = (props) => {
                 // 'Content-Type': 'application/x-www-form-urlencoded',
             },
         });
-        //   const json=  response.json(); // parses JSON response into native JavaScript objects
+        // const json=  response.json(); // parses JSON response into native JavaScript objects
         const json = await response.json();
-        console.log(json);
         setNotes(json);
     };
 
-    // Add a note
+
+    // ADD a note
     const addNote = async (title, description, tag) => {
         // to do api call
         // eslint-disable-next-line
@@ -41,9 +41,8 @@ const NoteState = (props) => {
             },
             body: JSON.stringify({ title, description, tag }), // body data type must match "Content-Type" header
         });
-        //   const json=  response.json(); // parses JSON response into native JavaScript objects
+        const json = await response.json(); // parses JSON response into native JavaScript objects
 
-        console.log("Adding a new note");
         const note = {
             _id: "63fa70d76e0c2a7d3bf25920",
             user: "63f8943bdc6315def59db027",
@@ -56,7 +55,8 @@ const NoteState = (props) => {
         setNotes(notes.concat(note)); //.concat returns new array
     };
 
-    // delete a note
+
+    // DELETE a note
     const deleteNote = async (id) => {
         const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
             //localhost:8000/api/notes/updatenote/63f8adb7c564b32089e7cd36
@@ -69,22 +69,22 @@ const NoteState = (props) => {
             },
         });
         const json = response.json(); // parses JSON response into native JavaScript objects
-        console.log(json);
-        console.log("deleting" + id);
+
         let newNotes = notes.filter((note) => {
             return note._id !== id;
         }); // creates a new array and returns all elements except element who has id === _id.
         setNotes(newNotes); //.concat returns new array
     };
 
-    // edit a note
+
+    // EDIT a note
     const editNote = async (id, title, description, tag) => {
         // eslint-disable-next-line
-        
+
         // API call
         const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
             //localhost:8000/api/notes/updatenote/63f8adb7c564b32089e7cd36
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            method: "PUT", // *GET, POST, PUT, DELETE, etc.
             headers: {
                 "Content-Type": "application/json",
                 "auth-token":
@@ -94,19 +94,22 @@ const NoteState = (props) => {
             body: JSON.stringify({ title, description, tag }), // body data type must match "Content-Type" header
         });
         // eslint-disable-next-line
-        const json = response.json(); // parses JSON response into native JavaScript objects
-
+        const json = await response.json(); // parses JSON response into native JavaScript objects
+        let newNotes = JSON.parse(JSON.stringify(notes))
         // Logic to edit in client
-        for (let index = 0; index < notes.length; index++) {
-            const element = notes[index];
+        for (let index = 0; index < newNotes.length; index++) {
+            const element = newNotes[index];
             if (element._id === id) {
-                element.title = title;
-                element.description = description;
-                element.tag = tag;
+                newNotes[index].title = title;
+                newNotes[index].description = description;
+                newNotes[index].tag = tag;
+                break;
             }
         }
+        setNotes(newNotes);
     };
 
+    
     return (
         <NoteContext.Provider
             value={{ notes, addNote, editNote, deleteNote, getAllNotes }}
